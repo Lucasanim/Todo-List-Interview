43d1a479b2ed39e525c68deb762305af7d666b1f
lucas <lucasanim12@gmail.com>
2021-08-26 12:47:14 -0300

    Deploy 2

--- backend/index.js
+++ backend/index.js
@@ -31,12 +31,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "..", "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    )
   );
 }
 
